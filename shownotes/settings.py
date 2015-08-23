7d549865351bb8aeea7e4b3fef06ae486cc3f53e
luto <m@luto.at>
2015-08-23 14:56:48 +0200

    raise an error if local settings cannot be loaded

--- shownotes/settings.py
+++ shownotes/settings.py
@@ -174,4 +174,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 try:
     from .local_settings import *
 except ImportError:
-    pass
+    print('could not load local settings from local_settings.py')
+    raise
