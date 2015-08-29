db31e8ea62383e3e84065c573eaf91487ebca604
Dr4k3 <dr4k3@gmx.net>
2015-08-29 20:28:54 +0200

    Removed private episodes dummy

--- snotes20/views/__init__.py
+++ snotes20/views/__init__.py
@@ -6,4 +6,3 @@ from .ImporterLogs import ImporterLogViewSet
 from .EditorViewSet import EditorViewSet
 from .ArchiveViewSet import ArchiveViewSet
 from .PodcastViewSet import PodcastViewSet
-from .PrivateEpisodeViewSet import PrivateEpisodeViewSet
