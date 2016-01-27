7bcd8a0b97b457d6cd2b17fd2b9559eb78de8819
Dr4k3 <Dr4k3@fastreboot.de>
2016-01-27 20:17:16 +0100

    Extended statistics views in django admin

--- statistic/admin.py
+++ statistic/admin.py
@@ -4,8 +4,23 @@ from django.contrib import admin
 from statistic import models
 from django.contrib import admin
 
+
 @admin.register(models.WordFrequency)
-class NotifyList(admin.ModelAdmin):
+class WordfrequencyAdmin(admin.ModelAdmin):
     fields = ('word', 'frequency', 'episode')
     list_display = ('word', 'frequency', 'episode')
     list_filter = ('word', 'frequency', 'episode')
+
+
+@admin.register(models.SignificantPodcastWords)
+class SignificantPodcastWordAdmin(admin.ModelAdmin):
+    fields = ('word', 'significance', 'podcast')
+    list_display = ('word', 'significance', 'podcast')
+    list_filter = ('word', 'significance', 'podcast')
+
+
+@admin.register(models.PodcastCosineSimilarity)
+class PodcastCosineSimilarityAdmin(admin.ModelAdmin):
+    fields = ('podcastx', 'podcasty', 'cosine_sim')
+    list_display = ('podcastx', 'podcasty', 'cosine_sim')
+    list_filter = ('podcastx', 'podcasty')
